0f40b1f457f6fb2e45867eb4b912ff3242b306c0
Deekerubo <dianakerubo24@gmail.com>
2021-06-02 18:04:04 +0300

    Add allure reporter

--- pageObjects/home.page.js
+++ pageObjects/home.page.js
@@ -2,15 +2,9 @@ const Page = require('../pageObjects/page')
 
 class HomePage extends Page {
     get CreateAContract () { return $('p=Create a Contract') }
-    get logoutButton () { return $('[data-original-title="Logout"]')}
     async clickCreateAContract () {
         await (await this.CreateAContract).click();
     }
-
-    async logout () {
-        await (await this.logoutButton).click();
-    }
-
 }
 
 module.exports = new HomePage();
